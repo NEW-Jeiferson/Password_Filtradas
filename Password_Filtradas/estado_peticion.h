470b917080677d7d0afa65cb79f55f0aa647a53d
David <jeffersonpaez5@gmail.com>
2026-07-02 11:07:33 -0400

    Agregando comentarios parte 2

--- Password_Filtradas/estado_peticion.h
+++ Password_Filtradas/estado_peticion.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <microhttpd.h>
 
+/* Estructura para mantener el estado de una petición HTTP. */
 typedef struct {
     char* buffer;
     size_t  tam_usado;
@@ -14,10 +15,11 @@ typedef struct {
 EstadoPeticion* crear_estado_peticion(void);
 void destruir_estado_peticion(EstadoPeticion* estado);
 
-
+/* Función para acumular datos recibidos en la petición HTTP. */
 int acumular_datos(EstadoPeticion* estado, const char* datos,
     size_t tam, size_t limite);
 
+/* Función de callback para manejar la finalización de la petición HTTP. */
 void solicitud_completada(void* cls,
     struct MHD_Connection* conexion,
     void** con_cls,
